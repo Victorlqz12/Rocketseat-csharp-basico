b3839ab6a233bab50e86766a8e50f480666f9ccc
victor alves da silva <victor.alves951@hotmail.com>
2025-08-17 10:41:04 -0300

    Add files via upload

---
+++ Atividade 2 Exibirnome/Program.cs
@@ -0,0 +1,13 @@
+﻿class program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Digite seu primeiro nome:");
+        string PrimeiroNome = Console.ReadLine();
+        Console.WriteLine("Digite seu sobrenome:");
+        string Sobrenome = Console.ReadLine();
+
+        Console.WriteLine($"Seu nome completo é: {PrimeiroNome} {Sobrenome}");
+    }
+}
+
