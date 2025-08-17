2621234dbeb29481309d670b0b2ddbc8c1d2a22b
victor alves da silva <victor.alves951@hotmail.com>
2025-08-17 10:39:37 -0300

    Add files via upload

---
+++ Atividade 1/Program.cs
@@ -0,0 +1,12 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        
+        Console.WriteLine("Digite o seu primeiro nome:");
+        string primeiroNome = Console.ReadLine();
+
+        Console.WriteLine("Olá, " + primeiroNome + "! Seja muito bem-vindo!");
+        
+    }
+}
